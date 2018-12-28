2bdb7af78eb987e0d6cd147fad90bdc8002b6dcf
Stefan Krüger <skrueger@email.de>
2018-12-28 01:12:34 +0100

    start 02

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ df = pd.DataFrame({'feature_1': range(1, N + 1, 1),
 df['labels'] = df['feature_1']*2
 print(df)
 
-df.to_csv(f'./source/df.csv', header=True)
+df.to_csv(f'./source/df.csv', header=True, index=False)
 
 ndarray1 = fc.make_ndarray_from_dataframe(df, ['feature_1', 'feature_2', 'labels'])
 print(ndarray1)
